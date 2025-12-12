dd62763dd7a7e7e34575791cc7edac9ed54802f5
hemadrikhandelwal <hemadrikhandelwal@gmail.com>
2025-12-12 21:43:00 +0530

    add addition details

--- DSA/maths/countDigit.js
+++ DSA/maths/countDigit.js
@@ -8,6 +8,8 @@ function findTotalDigits(n){
         count++;
         n = Math.floor(n/10);
     }
+
+    //note: math.floor as it will be in decimals
     console.log(count);
 
 }
@@ -19,4 +21,10 @@ function findTotalDigits(n){
     let count = Math.floor(Math.log10(n)+1);
     console.log(count);
 }
+
+/*Time Complexity - O(log base10 (N))
+ if you divide my 2 so it will turn to be O(log base2(N))
+ if you are writing a logic and number of iteraction is based on division  time complexity will be logritum and 
+ you are dividing an number by 10/5/2 
+ so its Time complexity will me logbase of that number.*/
 
